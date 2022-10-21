df5394a541fca311f8ddad5bf6df8c06d59f7981
Adrian Stevens <adrian.stevens@gmail.com>
2022-10-20 21:24:50 -0700

    Cleanup + fixes

---
+++ src/Gitfo/Options.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine.Text;
+using CommandLine;
+
+namespace Gitfo
+{
+    internal class Options
+    {
+        [Option('f', "fetch", Required = false, HelpText = "Fetch all repos")]
+        public bool Fetch { get; set; }
+    }
+}
--- src/Gitfo/Program.cs
+++ src/Gitfo/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CommandLine;
+using Gitfo;
 using LibGit2Sharp;
 
 Console.BackgroundColor = ConsoleColor.Black;
+
+const int NameWidth = 30;
 
 Console.WriteLine("| Hello Gitfo");
 
@@ -9,12 +13,21 @@ string path;
 
 int folderCount = 0;
 
+Parser.Default.ParseArguments<Options>(args)
+                  .WithParsed<Options>(o =>
+                  {
+                      if (o.Fetch)
+                      {
+     
+                      }
+                  });
+
 //update to add -f --fetch as a command line param
 //update to add -C --directory as a param
 //update to add -c --checkout 
 //update to add -p --pull
 //update ot add -v --verion
-if(args.Length > 0)
+if (args.Length > 0)
 {
     //assume the arg is the path
     path = args[0];
@@ -26,8 +39,8 @@ else
 
 var folders = Directory.GetDirectories(path);
 
-Console.WriteLine("| Repo name                      | Current branch       | Ahead  | Behind | Dirty |");
-Console.WriteLine("|--------------------------------|----------------------|--------|--------|-------|");
+Console.WriteLine("| Repo name                      | Current branch                 | Ahead  | Behind | Dirty |");
+Console.WriteLine("|--------------------------------|--------------------------------|--------|--------|-------|");
 
 foreach (var folder in folders)
 {
@@ -37,15 +50,22 @@ foreach (var folder in folders)
         {
             foreach (Remote remote in repo.Network.Remotes)
             {
-                FetchOptions options = new FetchOptions();
-                IEnumerable<string> refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
-                Commands.Fetch(repo, remote.Name, refSpecs, options, "");
+                try
+                {
+                    FetchOptions options = new FetchOptions();
+                    IEnumerable<string> refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
+                    Commands.Fetch(repo, remote.Name, refSpecs, options, "");
+                }
+                catch
+                {
+
+                }
             }
 
           //  int delta = repo.Head.TrackedBranch.Commits.Count() - repo.Head.Commits.Count();
 
-            var name = Path.GetFileName(folder).PadRight(30);
-            var friendly = repo.Head.FriendlyName.PadRight(20);
+            var name = Path.GetFileName(folder).PadRight(NameWidth);
+            var friendly = repo.Head.FriendlyName.PadRight(NameWidth);
             var ahead = $"{repo.Head.TrackingDetails.AheadBy}".PadRight(6);
             var behind = $"{repo.Head.TrackingDetails.BehindBy}".PadRight(6);
             var dirty = $"{repo.RetrieveStatus().IsDirty}".PadRight(5);
@@ -55,18 +75,22 @@ foreach (var folder in folders)
             Console.Write("| ");
             ConsoleWriteWithColor(name, ConsoleColor.White);
 
-            ConsoleWriteWithColor(friendly, repo.Head.IsTracking?ConsoleColor.White:ConsoleColor.Red);
-            ConsoleWriteWithColor(ahead, ConsoleColor.White);
-            ConsoleWriteWithColor(behind, ConsoleColor.White);
+            ConsoleWriteWithColor(friendly, ahead[0] == ' ' ? ConsoleColor.Yellow:ConsoleColor.White);
+            ConsoleWriteWithColor(ahead, ahead[0] == '0' ? ConsoleColor.White : ConsoleColor.Cyan);
+            ConsoleWriteWithColor(behind, behind[0] == '0' ? ConsoleColor.White : ConsoleColor.Cyan);
             ConsoleWriteWithColor(dirty, repo.RetrieveStatus().IsDirty?ConsoleColor.Red:ConsoleColor.White);
             Console.WriteLine();
 
             folderCount++;
         }
     }
+    catch(RepositoryNotFoundException ex)
+    {
+
+    }
     catch (Exception ex)
     {
-    //    Console.WriteLine($"Exception: {ex}");
+        Console.WriteLine($"Exception: {folder} - {ex.Message}");
     }
 }
 
@@ -78,6 +102,11 @@ if (folderCount == 0)
 
 void ConsoleWriteWithColor(string text, ConsoleColor color)
 {
+    if(text.Length > NameWidth)
+    {
+        text = text.Substring(0, NameWidth - 3) + "...";
+    }
+
     Console.ForegroundColor = color;
     Console.Write(text);
     Console.ForegroundColor = ConsoleColor.White;
